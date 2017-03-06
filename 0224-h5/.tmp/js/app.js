57b797ef5363b912150fa34775729f53505e99ea
zanjs <root@zanjs.com>
2017-03-06 10:18:37 +0800

    panlisale.aspx :pray:

--- 0224-h5/.tmp/js/app.js
+++ 0224-h5/.tmp/js/app.js
@@ -2339,23 +2339,6 @@
     window.onload = function () {
 
 
-        var tabNav = PD("#tab-nav");
-        // // Display an alert notification
-        // notyf.alert('You must fill out the form before moving forward');
-
-        // // Display a success notification
-        // notyf.confirm('Your changes have been successfully saved!');
-        tabNav.on("click", ".flex-n1", function () {
-            tabNav.find("a").removeClass("on")
-            var _t = PD(this);
-            var floor = _t.attr("floor")
-            _t.addClass("on")
-
-            PD(".floor").hide()
-
-            PD("#floor-"+ floor).show();
-
-        })
-
+       
     }
 })()
