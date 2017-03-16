d9e294b454345a666ff54a03ea652a67fe70bf60
zanjs <root@zanjs.com>
2017-03-16 18:42:02 +0800

    Every noble work is at first impossible :chestnut:

--- 0224-h5/src/js/main.js
+++ 0224-h5/src/js/main.js
@@ -2,14 +2,14 @@
 
 // var notyf = new Notyf();
 
-;
-(function () {
-    // Create an instance of Notyf
-    // var notyf = new Notyf(); 
+// ;
+// (function () {
+//     // Create an instance of Notyf
+//     // var notyf = new Notyf(); 
 
-    window.onload = function () {
+//     window.onload = function () {
 
-
+ 
        
-    }
-})()
+//     }
+// })()
